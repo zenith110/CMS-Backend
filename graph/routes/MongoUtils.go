f7933664be852a244fdb54f4bab147fa4542e24c
zenith110 <thequake@live.com>
2023-03-20 11:37:50 -0400

    Updated mongouri to work properly

--- graph/routes/MongoUtils.go
+++ graph/routes/MongoUtils.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -18,10 +19,13 @@ func ConnectToMongo() *mongo.Client {
 	}
 	mongoURI := os.Getenv("MONGOURI")
 	// Set client options
-	clientOptions := options.Client().ApplyURI(mongoURI)
-
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().ApplyURI(mongoURI).
+		SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
